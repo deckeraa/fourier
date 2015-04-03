1d4ef5f2de2b9e590458eab6e8b60077ddb78e40
john <decker49@gmail.com>
2015-04-03 17:05:10 -0500

    Got a basic sine series graphing. Still need to generalize.

--- src/fourier/core.clj
+++ src/fourier/core.clj
@@ -1,6 +1,29 @@
-(ns fourier.core)
+(ns fourier.core
+  (:require [incanter
+             [charts :as charts]
+             [core :as core]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn an1 [n]
+  (/ 4 (* Math/PI (- (* 2 n) 1))))
+
+(defn sine-term [n x]
+  (let [T 2
+        an an1]
+  (* (an n) (Math/sin (/ (* n Math/PI x) T)))))
+
+(sine-term 1 2)
+
+(defn sine-terms [x]
+  (eval (cons + (map #(sine-term % x) (range 1 10000)))))
+
+
+(sine-terms 3)
+
+;; execute the following line to make the graph
+(core/view (charts/function-plot sine-terms -11 11))
+
+;(defn fourier [a0 an bn num-iters]
+;  3)
+
+;((fn [x] (+ (Math/sin x) (Math/sin x))) (/ Math/PI 2))
+
