4199f38fc917cdb1cbe4b6de0896f0f2984793ed
john <decker49@gmail.com>
2015-04-04 12:31:01 -0500

    Got the abstraction I wanted.

--- src/fourier/core.clj
+++ src/fourier/core.clj
@@ -3,27 +3,31 @@
              [charts :as charts]
              [core :as core]]))
 
-(defn an1 [n]
-  (/ 4 (* Math/PI (- (* 2 n) 1))))
+(defn bn1 [n]
+  (/ 8 (* Math/PI (- (* 2 n) 1))))
 
-(defn sine-term [n x]
-  (let [T 2
-        an an1]
-  (* (an n) (Math/sin (/ (* n Math/PI x) T)))))
+(defn cosine-term [T an n x]
+  (* (an n) (Math/cos (/ (* n Math/PI x) T))))
 
-(sine-term 1 2)
+(defn sine-term [T bn n x]
+  (* (bn n) (Math/sin (/ (* n Math/PI x) T))))
 
-(defn sine-terms [x]
-  (eval (cons + (map #(sine-term % x) (range 1 10000)))))
+(defn cosine-terms [T an num-iters x]
+  (eval (cons + (map #(cosine-term T an % x) (range 1 (+ num-iters 1))))))
 
+(defn sine-terms [T bn num-iters x]
+  (eval (cons + (map #(sine-term T bn % x) (range 1 (+ num-iters 1))))))
 
-(sine-terms 3)
+(defn fourier [T a0 an bn num-iters x]
+  (let [an (if (nil? an) (fn [x] 0) an)
+        bn (if (nil? bn) (fn [x] 0) bn)]
+    (+ a0 (cosine-terms T an num-iters x) (sine-terms T bn num-iters x))))
+
+(defn view-fourier-graph [fourier-series]
+  (core/view (charts/function-plot fourier-series -10 10)))
+
+;; ((partial fourier 5 0 nil bn1 30) 3) ;; computing using the fourier series
 
 ;; execute the following line to make the graph
-(core/view (charts/function-plot sine-terms -11 11))
-
-;(defn fourier [a0 an bn num-iters]
-;  3)
-
-;((fn [x] (+ (Math/sin x) (Math/sin x))) (/ Math/PI 2))
+(view-fourier-graph (partial fourier 5 0 nil bn1 30))
 
