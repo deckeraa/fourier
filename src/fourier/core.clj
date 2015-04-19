34a6629c4c2d614e622465748cb92d6ec2361b07
john <decker49@gmail.com>
2015-04-19 01:56:54 -0500

    Fixed a bug where a_0 was not getting divided by 2 in the Fourier series solution.

--- src/fourier/core.clj
+++ src/fourier/core.clj
@@ -13,13 +13,35 @@
     0
     (/ 4 (* n Math/PI))))
 
-;; (defn bn1 [n]
-;;   (/ 8 (* Math/PI (- (* 2 n) 1))))
+(defn bn2_b [n]
+  (if (even? n)
+    (/ 4 (* n Math/PI))
+    0))
 
 (defn an2_b [n]
   (if (even? n)
     0
     (/ 8 (* n n Math/PI Math/PI))))
+
+(defn an2_c [n]
+  (* (/ 2 (* n Math/PI))
+     (- (Math/sin (/ (* 2 n Math/PI) 3))
+        (Math/sin (/ (* 1 n Math/PI) 3)))))
+
+(defn bn2_c [n]
+  (* (/ 2 (* -1 n Math/PI))
+     (- (Math/cos (/ (* 2 n Math/PI) 3))
+        (Math/cos (/ (* 1 n Math/PI) 3)))))
+
+(defn bn_5 [n]
+  (if (odd? n)
+    (/ 400 (* n Math/PI))
+    0))
+
+(defn an_5 [n]
+  (if (odd? n)
+    (/ -400 (* n n Math/PI Math/PI))
+    0))
 
 ; Compute an individual cosine term: a_n * cos( (n pi x)/T )
 (defn cosine-term [T an n x]
@@ -45,11 +67,16 @@
 ; Plots the Fourier series
 ; fourier-series needs to be a function that takes in one parameter, x
 (defn view-fourier-graph [fourier-series]
-  (core/view (charts/function-plot fourier-series -10 10)))
+  (core/view (charts/function-plot fourier-series -5 5)))
 
 ;; ((partial fourier 5 0 nil bn1 30) 3) ;; computing using the fourier series
 
 ;; execute the following line to make the graph
-(view-fourier-graph (partial fourier 5 0 nil bn1 30))
-(view-fourier-graph (partial fourier 2 0 an2_b nil 100))
+;; (view-fourier-graph (partial fourier 5 0 nil bn1 10))
+;; (view-fourier-graph (partial fourier 2 0 an2_b nil 10))
+;; (view-fourier-graph (partial fourier 2 0 nil bn2_b 20))
+;; (view-fourier-graph (partial fourier 3 0 an2_c nil 20))
+;; (view-fourier-graph (partial fourier 3 0 nil bn2_c 30))
+;; (view-fourier-graph (partial fourier 40 0 nil bn_5 30))
+(view-fourier-graph (partial fourier 50 0 an_5 nil 30))
 
